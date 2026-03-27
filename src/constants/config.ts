899729faf457c750a6bee835d34f659e3a031f9b
hamoodtechit <hamoodtechit@gmail.com>
2026-03-27 17:57:11 +0600

    api url change

--- src/constants/config.ts
+++ src/constants/config.ts
@@ -4,7 +4,7 @@ export const config = {
     url: process.env.NEXT_PUBLIC_APP_URL || "http://localhost:3000",
   },
   api: {
-    baseUrl: process.env.NEXT_PUBLIC_API_URL || "https://hamood-erp.ourb.live/api",
+    baseUrl: process.env.NEXT_PUBLIC_API_URL || "https://erp-srv.hamoodtech.com/api",
     timeout: 30000,
   },
   i18n: {
